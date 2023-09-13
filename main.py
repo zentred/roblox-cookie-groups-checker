17f06d569576f2f4d65b905949852e0ba358007d
zentred <87871486+zentred@users.noreply.github.com>
2023-09-13 23:11:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,109 +1,125 @@
 import requests, json, time, ctypes, random
+from threading import Thread
 from rich import print
+
+proxies = open('proxies.txt').read().splitlines()
+all_cookies = open('files/cookies.txt').read().splitlines()
 
 class Bot:
     def __init__(self):
         self.cookies = open('cookies.txt').read().splitlines()
-        self.group_log = {}
+        self.data = {}
 
-    def ratelimited(self, reason):
-        for i in range(60):
-            ctypes.windll.kernel32.SetConsoleTitleW(f'Ratelimited for: {reason} - {i+1}/60')
-            time.sleep(1)
+    def check_cookies(self):
+        with requests.Session() as session:
+            for cookie in self.cookies:
+                session.cookies['.ROBLOSECURITY'] = cookie
+                response = session.get(
+                    'https://www.roblox.com/my/settings/json'
+                )
 
-    def start_process(self):
-        for cookie in self.cookies:
-            response = requests.get(
-                'https://www.roblox.com/my/settings/json',
-                cookies = {'.ROBLOSECURITY': cookie}
-            )
-            if response.status_code == 200:
-                userid = response.json()['UserId']
-                groups = self.owned_groups(userid)
-                if groups != None:
-                    print(f'[green]{userid} owns {len(groups)} groups')
-                    self.group_log[str(userid)] = {'totalFunds': 0, 'totalVisits': 0, 'totalPending': 0, 'groups': {}}
-                    self.check_funds(cookie, groups, userid)
+                if response.status_code == 200:
+                    userid = str(response.json()['UserId'])
+                    self.data[userid] = {'totalRobux': 0, 'totalPending': 0, 'totalAssets': 0, 'totalVisits': 0, 'totalMembers': 0, 'groups': {}}
+                    groups = self.owned_groups(userid)
+
+                    checked = 0
+                    for group in groups:
+                        groupid = str(group["group"]["id"])
+                        self.data[userid]['groups'][groupid] = {'robux': 0, 'assets': 0, 'visits': 0, 'members': group['group']['memberCount'], 'name': group["group"]["name"], 'games': {}}
+                        self.check_robux(userid, groupid, session)
+                        self.check_revenue(userid, groupid, session)
+                        self.check_games(userid, groupid, session)
+                        self.check_assets(userid, groupid, session)
+                        checked += 1
+                        ctypes.windll.kernel32.SetConsoleTitleW(f'{checked}/{len(groups)}')
+
+                    # -------------------- this section saves groups as single line without indent, alot easier to read
+                    group_list = []
+                    for group in self.data[userid]['groups']:
+                        group_list.append(str(self.data[userid]['groups'][group]))
+                    self.data[userid]['groups'] = group_list
                     with open('log.json', 'w') as file:
-                        json.dump(self.group_log, file, indent=4)
-                else:
-                    print(f'[yellow]{userid} owns no groups')
-            else:
-                print(f'[red]Invalid cookie: {cookie}')
+                        json.dump(self.data, file, indent=4)
+                    # -------------------- delete this section if you want to save the file as a proper dict
 
     def owned_groups(self, userid):
-        response = requests.get(
-            f'https://groups.roblox.com/v2/users/{userid}/groups/roles'
-        )
-        if response.status_code == 200:
-            return [group for group in response.json()['data'] if group['role']['rank'] == 255]
-
-    def check_funds(self, cookie, groups, userid):
-        session = requests.Session()
-        session.cookies['.ROBLOSECURITY'] = cookie
-        checked_groups = 0
-
-        for group in groups:
-            self.group_log[str(userid)]['groups'][str(group["group"]["id"])] = {'name': group["group"]["name"], 'robux': None}
-            while 1:
-                response = session.get(
-                    f'https://economy.roblox.com/v1/groups/{group["group"]["id"]}/currency'
-                )
-                if response.status_code == 200:
-                    checked_groups += 1
-                    ctypes.windll.kernel32.SetConsoleTitleW(f'Funds checked: {checked_groups}/{len(groups)}')
-                    self.group_log[str(userid)]['groups'][str(group["group"]["id"])]['robux'] = response.json()['robux']
-                    self.group_log[str(userid)]['totalFunds'] += response.json()['robux']
-                    break
+        return [
+            group
+            for group in requests.get(
+                f'https://groups.roblox.com/v2/users/{userid}/groups/roles'
+            ).json()['data']
+            if group['role']['rank'] == 255
+        ]
+    
+    def check_robux(self, userid, groupid, session):
+        while 1:
+            response = session.get(
+                f'https://economy.roblox.com/v1/groups/{groupid}/currency'
+            )
+            if response.status_code == 200:
+                self.data[userid]['groups'][groupid]['robux'] = response.json()['robux']
+                self.data[userid]['totalRobux'] += response.json()['robux']
+                break
+            else:
+                if response.json()['errors'][0]['message'] == 'Too many requests':
+                    time.sleep(5)
                 else:
-                    if response.json()['errors'][0]['message'] == 'Too many requests':
-                        self.ratelimited('checking funds')
-                    else: break
-        self.all_revenue(session, groups, userid)
-        self.check_games(session, groups, userid)
-
-    def all_revenue(self, session, groups, userid):
-        checked_groups = 0
-        for group in groups:
-            while 1:
-                response = session.get(
-                    f'https://economy.roblox.com/v1/groups/{group["group"]["id"]}/revenue/summary/day',
-                )
-                if response.status_code == 200:
-                    checked_groups += 1
-                    ctypes.windll.kernel32.SetConsoleTitleW(f'Revenue checked: {checked_groups}/{len(groups)}')
-                    if any(response.json()[k] not in [False, 0] for k in response.json()):
-                        self.group_log[str(userid)]['groups'][str(group["group"]["id"])]['revenue'] = {}
-                    for k in response.json():
-                        self.group_log[str(userid)]['totalPending'] += response.json()['pendingRobux']
-                        if response.json()[k]:
-                            self.group_log[str(userid)]['groups'][str(group["group"]["id"])]['revenue'][k] = response.json()[k]
                     break
-                else:
-                    if response.json()['errors'][0]['message'] == 'Too many requests':
-                        self.ratelimited('checking revenue')
-                    else: break
 
-    def check_games(self, session, groups, userid):
-        checked_groups = 0
-        for group in groups:
-            while 1:
-                response = session.get(
-                    f'https://games.roblox.com/v2/groups/{group["group"]["id"]}/games?limit=100&sortOrder=Asc',
-                )
-                if response.status_code == 200:
-                    checked_groups += 1
-                    ctypes.windll.kernel32.SetConsoleTitleW(f'Games checked: {checked_groups}/{len(groups)}')
-                    if len(response.json()['data']):
-                        self.group_log[str(userid)]['groups'][str(group["group"]["id"])]['games'] = {}
-                    for game in response.json()['data']:
-                        self.group_log[str(userid)]['groups'][str(group["group"]["id"])]['games'][str(game['id'])] = {'name': game['name'], 'visits': game['placeVisits']}
-                        self.group_log[str(userid)]['totalVisits'] += game['placeVisits']
-                    break
+    def check_revenue(self, userid, groupid, session):
+        while 1:
+            response = session.get(
+                f'https://economy.roblox.com/v1/groups/{groupid}/revenue/summary/day',
+            )
+            if response.status_code == 200:
+                if any(response.json()[k] not in [False, 0] for k in response.json()):
+                    self.data[userid]['groups'][groupid]['revenue'] = {}
+                for k in response.json():
+                    if k == 'pendingRobux':
+                        self.data[userid]['totalPending'] += response.json()['pendingRobux']
+                    if response.json()[k]:
+                        self.data[userid]['groups'][groupid]['revenue'][k] = response.json()[k]
+                break
+            else:
+                if response.json()['errors'][0]['message'] == 'Too many requests':
+                    time.sleep(5)
                 else:
-                    if response.json()['errors'][0]['message'] == 'Too many requests':
-                        self.ratelimited('checking games')
-                    else: break
+                    break
+
+    def check_games(self, userid, groupid, session):
+        while 1:
+            response = session.get(
+                f'https://games.roblox.com/v2/groups/{groupid}/games?limit=100&sortOrder=Asc&accessFilter=Public',
+            )
+            if response.status_code == 200:
+                if len(response.json()['data']):
+                    self.data[userid]['groups'][groupid]['games'] = {}
+                for game in response.json()['data']:
+                    self.data[userid]['groups'][groupid]['games'][str(game['id'])] = {'name': game['name'], 'visits': game['placeVisits']}
+                    self.data[userid]['groups'][groupid]['visits'] += game['placeVisits']
+                    self.data[userid]['totalVisits'] += game['placeVisits']
+                break
+            else:
+                if response.json()['errors'][0]['message'] == 'Too many requests':
+                    time.sleep(5)
+                else:
+                    break
+
+    def check_assets(self, userid, groupid, session):
+        cursor, assets = '', []
+        while cursor != None:
+            response = session.get(
+                f'https://catalog.roblox.com/v1/search/items?category=All&creatorTargetId={groupid}&creatorType=Group&cursor={cursor}&limit=100&sortOrder=Desc'
+            )
+            if response.status_code == 200:
+                for asset in response.json()['data']:
+                    if asset['itemType'] == 'Asset':
+                        assets.append(asset['id'])
+                cursor = response.json()['nextPageCursor']
+            else: break
+        self.data[userid]['groups'][groupid]['assets'] += len(assets)
+        self.data[userid]['totalAssets'] += len(assets)
 
 b = Bot()
-b.start_process()
+b.check_cookies()
